5427deefce515d9f628895165c242997e2f3374a
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-08-23 20:37:33 +0500

    updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,12 +6,15 @@ this.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheData).then(cache => {
             cache.addAll([
+                'https://fonts.googleapis.com/css2?family=Big+Shoulders+Display&display=swap',
                 '/static/js/bundle.js',
                 '/static/js/0.chunk.js',
                 '/static/js/main.chunk.js',
+                '/static/js/vendors~main.chunk.js',
                 '/favicon.ico',
-                '/logo192.svg',
+                '/logo192.png',
                 '/index.html',
+                '/manifest.json',
                 '/'
             ])
         }).catch(error => {
