819a59fb80a40dd2173f83e708c35290c8d1389e
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-08-25 13:27:37 +0500

    updated

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,49 @@
-console.log('registered')
+const version = 'appv1'
 
-let cacheData = 'appv1'
+const cacheFiles = [
+    'https://fonts.googleapis.com/css2?family=Big+Shoulders+Display&display=swap',
+    '/static/css/main.a7842a67.chunk.css',
+    '/static/js/main.40ef5f63.chunk.js',
+    '/static/js/main.1d46bdfd.chunk.js',
+    '/static/js/2.11a1e67d.chunk.js',
+    '/manifest.json',
+    '/favicon.ico',
+    '/logo192.png',
+    '/'
+
+]
 
 this.addEventListener('install', e => {
     e.waitUntil(
-        caches.open(cacheData).then(cache => {
-            cache.addAll([
-                'https://fonts.googleapis.com/css2?family=Big+Shoulders+Display&display=swap',
-                '/static/js/bundle.js',
-                '/static/js/0.chunk.js',
-                '/static/js/main.chunk.js',
-                '/static/js/vendors~main.chunk.js',
-                '/favicon.ico',
-                '/logo192.png',
-                '/index.html',
-                '/manifest.json',
-                '/'
-            ])
-        }).catch(error => {
-            console.log('error', error)
+        caches.open(version).then(cache => {
+            return cache.addAll(cacheFiles)
         })
     )
 })
 
-this.addEventListener('fetch', e => {
+this.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+});
+
+const options = {
+    ignoreSearch: true,
+    ignoreMethod: true,
+    ignoreVary: true,
+};
+
+this.addEventListener('fetch', event => {
     if (!navigator.onLine) {
-        e.respondWith(
-            caches.match(e.request).then((result) => {
-                if (result) {
-                    return result
-                }
-            })
-        )
+        event.respondWith(
+            caches
+                .match(event.request, options)
+                .then((response) => {
+                    if (response) {
+                        return response || fetch.response;
+                    }
+                })
+                .catch((err) => {
+                    console.log('err', err);
+                })
+        );
     }
-})
+});
